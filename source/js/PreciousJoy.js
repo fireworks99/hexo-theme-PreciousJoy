4410a81b71b5b912eaadc6efb95e880ffd55adc5
fireworks99 <2639237361@qq.com>
2020-11-17 09:12:52 +0800

    hide search function in articles

--- source/js/PreciousJoy.js
+++ source/js/PreciousJoy.js
@@ -12,6 +12,12 @@ $(document).ready(function () {
     // 	$(this).html("<ol><li>" + $(this).html().replace(/\n/g,"\n</li><li>") +"\n</li></ol>");
     // });
     
+    if($('#details').length > 0){
+    	$('.menu-item-search').css({
+    		"display": "none"
+    	})
+    }
+    
 
     $('pre code').each(function(){
         var texts = $(this).text().split('\n');
